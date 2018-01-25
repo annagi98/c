c349001832be596ccee215d38e9ab8fc7917d0d3
nagianna <nagianna@github.com>
2018-01-25 22:53:55 +0100

    Projekt

---
+++ slowka_angielskie/main.c
@@ -0,0 +1,74 @@
+//main.c Anna Nagi
+
+#include <stdio.h>
+#include "projekt.h"
+
+int main(void){
+    printf("Program pomaga w nauce technicznego jezyka angelskiego.\n\n");
+    printf("Wybierz jezyk, w ktorym chcesz, aby wyswietlilo sie slowo. \n1-polski, 2-angielski\n\n");
+
+    enum wybieranie_jezyka jezyk=wczytaj_wybor_jezyka();
+
+    int liczba_zyc=5;
+    int liczba_podpowiedzi=3;
+    int punkty=0;
+
+    srand(time(NULL));
+    char *wybor_pliku_1[100];
+    char *wybor_pliku_2[100];
+
+    przypisanie_jezyka_danej_tablicy(wybor_pliku_1, wybor_pliku_2, jezyk);
+    int linie=policz_linie(wybor_pliku_1);
+
+    while(liczba_zyc>0){
+        int wylosowany_numer_linii = losowanie(linie);
+
+        const char *slowo_1=wyluskanie_slowa_z_pliku(wylosowany_numer_linii, wybor_pliku_1);
+        printf("\n\n\nWylosowano slowo: %s", slowo_1);
+        const char *slowo_2=wyluskanie_slowa_z_pliku(wylosowany_numer_linii, wybor_pliku_2);
+
+        liczba_podpowiedzi=czy_chcesz_skorzystac_z_podpowiedzi(liczba_podpowiedzi, slowo_2);
+
+        char slowo_uzytkownika[100];
+        const char tmp[10]="\n\0";
+        printf("\n\nPodaj to slowo w drugim jezyku:\n");
+        scanf("%s",slowo_uzytkownika);
+
+        int p=0;
+        char litera;
+        while (slowo_uzytkownika[p])
+        {
+            if (!isalpha(slowo_uzytkownika[p])){
+                printf("\n\nW twoim wyrazie wystepuja znaki inne niz litery. Wprowadz slowo ponownie: \n");
+                scanf("%s",slowo_uzytkownika);
+                p=0;
+            }
+            litera=slowo_uzytkownika[p];
+            slowo_uzytkownika[p]=(tolower(litera));
+            p++;
+        }
+
+        strcat(slowo_uzytkownika, tmp);
+
+        int rowne=porownywanie_slow(slowo_2, slowo_uzytkownika);
+        if (rowne==1){
+            printf("\nBrawo!");
+            punkty++;
+            printf("Zdobyles punkt!.\n");
+        }
+        if (rowne==0){
+            printf("\nPodales nieprawidlowe slowo. Tracisz zycie.\n");
+            liczba_zyc--;
+            printf("\nPozostale zycia:  ");
+            int j;
+            for(j=0; j<liczba_zyc; j++){
+                printf("x");
+            }
+
+            printf("\n");
+            printf("Poprawne slowo: %s\n", slowo_2);
+        }
+    }
+    printf("Gra skonczona! Zdobyles tyle punktow: %d\n", punkty);
+    return 0;
+}
---
+++ slowka_angielskie/projekt.c
@@ -0,0 +1,116 @@
+//projekt.c Anna Nagi
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "projekt.h"
+
+int wczytaj_wybor_jezyka(void){
+    int jezyk;
+    scanf("%d", &jezyk);
+        while(jezyk!=1 && jezyk!=2){
+            printf("Podales nieprawidlowa liczbe, wybierz 1 lub 2.\n");
+            scanf("%d", &jezyk);
+        }
+    return jezyk;
+}
+
+int policz_linie(char **wybor_pliku_1){
+    FILE *do_policzenia = fopen(*wybor_pliku_1, "r");
+    int liczba_linii = 0;
+    char c;
+    for (c = getc(do_policzenia); c != EOF; c = getc(do_policzenia)){
+        if (c == '\n'){
+            liczba_linii++;
+        }
+    }
+    fclose(do_policzenia);
+    return liczba_linii;
+}
+
+int losowanie(int linie){
+    int wylosowana=rand()%linie+1;
+    return wylosowana;
+}
+
+void przypisanie_jezyka_danej_tablicy(char *wybor_pliku_1[], char *wybor_pliku_2[], int jezyk){
+    if (jezyk==POLSKI_NA_ANGIELSKI){
+        printf("Wybrales losowanie slow w jezyku polskim.");
+        wybor_pliku_1[0] = "polskie.txt";
+        wybor_pliku_2[0] = "angielskie.txt";
+    }
+    if (jezyk==ANGIELSKI_NA_POLSKI){
+        printf("Wybrales losowanie slow w jezyku angielskim.");
+        wybor_pliku_1[0] = "angielskie.txt";
+        wybor_pliku_2[0] = "polskie.txt";
+    }
+}
+
+const char *wyluskanie_slowa_z_pliku(int wylosowany_numer_linii, char *wybor_pliku[]){
+    FILE *otwierany_plik = fopen(*wybor_pliku, "r");
+        int licznik1 = 0;
+
+        if (otwierany_plik!= NULL){
+            static char wylosowane_slowo[100];
+            while (fgets(wylosowane_slowo, sizeof wylosowane_slowo, otwierany_plik)!=NULL){
+                if (licznik1==wylosowany_numer_linii){
+                    break;
+                }
+                else{
+                    licznik1++;
+                }
+            }
+            fclose(otwierany_plik);
+            return wylosowane_slowo;
+        }
+
+        else{
+            fprintf( stderr, "Nie mozna wczytac pliku \n");
+            exit(1);
+        }
+}
+
+void wyswietlanie_podpowiedzi(const char* slowo_przetlumaczone){
+    int ilosc_liter_w_slowie=strlen(slowo_przetlumaczone);
+    int i;
+    printf("Slowo, ktore musisz wpisac ma tyle liter:  ");
+    for(i=0; i<ilosc_liter_w_slowie-1; i++){
+        printf("_");
+    }
+}
+
+int czy_chcesz_skorzystac_z_podpowiedzi(int liczba_podpowiedzi, const char* slowo_przetlumaczone){
+    int podpowiedz=2;
+    printf("\nCzy chcesz skorzystac z podpowiedzi?\n");
+    printf("\nIlosc podpowiedzi do wykorzystania: %d\n", liczba_podpowiedzi);
+    printf("1-tak, 2-nie   ");
+    scanf("%d", &podpowiedz);
+
+    while (podpowiedz!=1 && podpowiedz!=2){
+        printf("Podales niepoprawna liczbe, wpisz 1 lub 2.\n");
+        scanf("%d", &podpowiedz);
+    }
+
+    if (podpowiedz==1){
+        wyswietlanie_podpowiedzi(slowo_przetlumaczone);
+        liczba_podpowiedzi--;
+    }
+    return liczba_podpowiedzi;
+}
+
+
+int porownywanie_slow(const char* slowo_przetlumaczone, char* slowo_uzytkownika){
+    int czy_sa_rowne=0;
+
+    if(strcmp(slowo_przetlumaczone, slowo_uzytkownika)==0){
+        printf("\nBadane lancuchy znakow sa rowne.\n" );
+        czy_sa_rowne=1;
+    }
+    else{
+        printf( "Badane lancuchy znakow nie sa rowne.\n" );
+    }
+    return czy_sa_rowne;
+}
+
